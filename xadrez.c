017de21b17909829ba8318783fd8b6644c6a68b8
Sidvictor15 <sidneyvictor52@gmail.com>
2025-03-26 14:08:54 +0000

    edit

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main() {
 
     int movi = 1;
     int i = 1;
+    int k= 1;
 
 printf("Movimentação Torre:\n");
 
@@ -19,8 +20,8 @@ printf("Movimentação Bispo:\n");
 do
 {
     printf("Cima,Direita\n");
-    i++;
-} while (i <= 5);
+    movi++;
+} while (movi <= 5);
 
 
 printf("Movimentação Rainha:\n");
@@ -29,6 +30,30 @@ for (int i = 1; i <= 8; i++)
 {
     printf("Esquerda\n");
 }
+
+printf("Movimentação do Cavalo:\n");
+
+
+while (k > 0)
+{
+    for (int i = 1; i <= 2; i++)
+    {
+        printf("Cima\n");
+    }
+ printf("Direita\n"); 
+ k--;  
+}
+
+for (int i = 1; i > 0; i--)
+{
+    for (int j = 1; j <=2 ; j++)
+    {
+        printf("Cima\n");
+    }
+ printf("Direita\n");   
+}
+
+
 
    
 
